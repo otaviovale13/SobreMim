b3d78c96111cddc5ee05cebeb927a24dbbbc66b7
otaviovale13 <otaviovale13@gmail.com>
2025-03-17 13:49:48 -0300

    quase

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SobreMim/', // Coloque o nome do repositório aqui
+  base: '/SobreMim/', // Nome do repositório
 });
